1e10d55a4be2e07dcbd58a6bd3fe8b3e99c2a9f9
June Ha <june.ha14@gmail.com>
2021-04-12 14:58:00 -0700

    Add Section component

---
+++ src/components/Section.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Pressable, StyleProp, StyleSheet, ViewStyle } from "react-native";
+import { Box } from "./Box";
+import { Text } from "./Text";
+import { Colors, Spacing } from "./theme";
+
+interface Section {
+  title: string;
+  accessoryTitle?: string;
+  onAccessoryPress?: () => void;
+  style?: StyleProp<ViewStyle>;
+}
+
+export const Section: React.FC<Section> = ({
+  title,
+  accessoryTitle,
+  onAccessoryPress,
+  children,
+  style,
+}) => {
+  return (
+    <Box style={style}>
+      <Box style={styles.container}>
+        <Text style={styles.title} variant="sectionHeading">
+          {title}
+        </Text>
+        <Pressable style={styles.accessoryTitle} onPress={onAccessoryPress}>
+          {accessoryTitle && (
+            <Text variant="body" color={Colors.ActionPrimary}>
+              {accessoryTitle}
+            </Text>
+          )}
+        </Pressable>
+      </Box>
+      {children}
+    </Box>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: Spacing.s,
+  },
+  title: {
+    flexShrink: 1,
+  },
+  accessoryTitle: {
+    flexGrow: 1,
+    alignItems: "flex-end",
+  },
+});
