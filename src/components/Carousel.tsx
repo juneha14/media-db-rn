32eaaae7312fa01d1fb88355396069bfeb538161
June Ha <june.ha14@gmail.com>
2021-04-13 10:19:51 -0700

    Create Carousel component

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { FlatListProps, StyleProp, FlatList, ViewStyle } from "react-native";
+
+type CarouselFlatListProps<Item> = Pick<
+  FlatListProps<Item>,
+  | "keyExtractor"
+  | "data"
+  | "renderItem"
+  | "pagingEnabled"
+  | "showsHorizontalScrollIndicator"
+  | "snapToInterval"
+>;
+
+interface CarouselProps<Item> extends CarouselFlatListProps<Item> {
+  contentContainerStyle?: StyleProp<ViewStyle>;
+  style?: StyleProp<ViewStyle>;
+}
+
+export function Carousel<Item>({
+  keyExtractor,
+  data,
+  renderItem,
+  showsHorizontalScrollIndicator = false,
+  snapToInterval,
+  pagingEnabled = false,
+  contentContainerStyle,
+  style,
+}: CarouselProps<Item>): JSX.Element {
+  return (
+    <FlatList
+      keyExtractor={keyExtractor}
+      style={style}
+      contentContainerStyle={contentContainerStyle}
+      data={data}
+      renderItem={renderItem}
+      horizontal
+      showsHorizontalScrollIndicator={showsHorizontalScrollIndicator}
+      pagingEnabled={pagingEnabled}
+      snapToInterval={snapToInterval}
+      decelerationRate="fast"
+    />
+  );
+}
