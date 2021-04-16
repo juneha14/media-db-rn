32e0c92c1ed055252710b33810075082a48e5719
June Ha <june.ha14@gmail.com>
2021-04-16 08:13:26 -0700

    Prestorybook loader npm

---
+++ storybook/storyLoader.js
@@ -0,0 +1,19 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require("../src/components/Caption/Caption.stories");
+  require("../src/components/CaptionImage/CaptionImage.stories");
+}
+
+const stories = [
+  "../src/components/Caption/Caption.stories",
+  "../src/components/CaptionImage/CaptionImage.stories",
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};
