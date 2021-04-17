13ab53b693c61741f2ff1fc5dd4adf6559c7f9cd
June Ha <june.ha14@gmail.com>
2021-04-17 14:06:34 -0700

    PageHeader and Text component storybook restructure

--- src/components/Typography/PageHeader.tsx
+++ src/components/Typography/PageHeader.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { View, StyleSheet, StyleProp, ViewStyle } from "react-native";
+import { StyleSheet, StyleProp, ViewStyle } from "react-native";
+import { Box } from "../Box";
 import { Text } from "./Text";
-import { Spacing } from "./theme";
+import { Spacing } from "../theme";
 
 interface PageHeaderProps {
   title: string;
@@ -15,12 +16,12 @@ export const PageHeader: React.FC<PageHeaderProps> = ({
   style,
 }) => {
   return (
-    <View style={style}>
+    <Box style={style}>
       <Text style={styles.heading} variant="pageHeading">
         {title}
       </Text>
       {subtitle ? <Text variant="body">{subtitle}</Text> : null}
-    </View>
+    </Box>
   );
 };
 
