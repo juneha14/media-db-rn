7d6eddce78a922ccc6366ca7b96399782ba0c226
June Ha <june.ha14@gmail.com>
2021-05-03 13:14:49 -0700

    Add tab navigation routes

--- App.tsx
+++ App.tsx
@@ -1,15 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { Text, View } from "react-native";
+import { TabRoutes } from "./src/navigation";
 
-const App = () => {
-  return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-};
-
-export { default } from "./storybook";
-// export default App;
+// export { default } from "./storybook";
+export default TabRoutes;
---
+++ src/navigation/index.ts
@@ -0,0 +1,1 @@
+export { TabRoutes } from "./TabRoutes";
