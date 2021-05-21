ef58da58dce89600b2c57f0b18e7764760fd066a
June Ha <june.ha14@gmail.com>
2021-05-21 11:43:41 -0700

    Extract MediaList UI from MediaScreen into own component while leaving usePagination hook inside screen. This is so that we can reuse MediaList when showing Recommended screen from MediaDetails and it's not possible to use MediaScreen for both showing recommended and as a home screen

--- src/screens/Home/MediaScreen.tsx
+++ src/screens/Home/MediaScreen.tsx
@@ -1,20 +1,13 @@
-import React, { useCallback, useMemo, useRef } from "react";
-import { StyleSheet, Dimensions } from "react-native";
+import React, { useRef } from "react";
 import { FlatList } from "react-native-gesture-handler";
-import { useNavigation, useScrollToTop } from "@react-navigation/native";
-import { StackNavigationProp } from "@react-navigation/stack";
-import { DiscoverParamList } from "../../navigation";
+import { useScrollToTop } from "@react-navigation/native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { PaginatedList } from "../../components/PaginatedList";
-import { QueryContainer } from "../../components/QueryContainer";
-import { Colors, Spacing } from "../../components/theme";
 import { usePagination } from "../../hooks";
-import { Favourite, Movie } from "../../models";
-import { MediaCell, useFavouriteState } from "../shared";
+import { Movie } from "../../models";
+import { MediaList } from "../shared";
 
 export const MediaScreen: React.FC = () => {
   const { top } = useSafeAreaInsets();
-  const { push } = useNavigation<StackNavigationProp<DiscoverParamList>>();
 
   const listRef = useRef<FlatList<Movie> | null>(null);
   useScrollToTop(listRef);
@@ -30,96 +23,18 @@ export const MediaScreen: React.FC = () => {
     refresh,
   } = usePagination<Movie>("NowPlayingMovies", { page: 1 });
 
-  const { favourites, onToggleLike } = useFavouriteState();
-
-  const width = useMemo(() => {
-    // (screenWidth / 2) - (paddingHorizontal / 2 + marginHorizontal / 2)
-    return Dimensions.get("window").width / 2 - 15;
-  }, []);
-
-  const onSelectCell = useCallback(
-    (id: number) => push("MediaDetails", { id }),
-    [push]
-  );
-
-  const onSelectLike = useCallback(
-    (favourite: Favourite) => () => {
-      onToggleLike(favourite);
-    },
-    [onToggleLike]
-  );
-
-  const renderItem = useCallback(
-    ({
-      item: { id, posterPath, backdropPath, title, releaseDate, voteAverage },
-    }: {
-      item: Movie;
-    }) => {
-      return (
-        <MediaCell
-          style={styles.cellContainer}
-          id={id}
-          mediaImgType="poster"
-          mediaImgUrl={posterPath}
-          title={title}
-          releaseDate={releaseDate}
-          rating={voteAverage}
-          width={width}
-          isLiked={favourites.find((f) => f.id === id) !== undefined}
-          onPress={onSelectCell}
-          onLikePress={onSelectLike({
-            id,
-            posterPath,
-            backdropPath,
-            title,
-            releaseDate,
-            voteAverage,
-          })}
-        />
-      );
-    },
-    [width, onSelectCell, onSelectLike, favourites]
-  );
-
-  const keyExtractor = useCallback((item: Movie, index: number) => {
-    return String(item.id) + String(index);
-  }, []);
-
   return (
-    <QueryContainer
-      wrapperStyle="unwrapped"
-      isLoading={isLoading}
-      isErrored={errorMessage !== undefined}
-      onRetryQuery={() => refresh(true)}
-    >
-      <PaginatedList
-        style={styles.container}
-        listRef={(input) => (listRef.current = input)}
-        contentContainerStyle={[styles.contentContainer, { paddingTop: top }]}
-        isFetching={isFetching}
-        refreshing={isRefreshing}
-        keyExtractor={keyExtractor}
-        numColumns={2}
-        data={allData}
-        renderItem={renderItem}
-        onEndReached={() => fetchNextPage({ page: nextPage })}
-        onRefresh={() => refresh(false)}
-      />
-    </QueryContainer>
+    <MediaList
+      listRef={listRef}
+      loading={isLoading}
+      errored={errorMessage !== undefined}
+      onErrorRetry={() => refresh(true)}
+      refreshing={isRefreshing}
+      onRefresh={() => refresh(false)}
+      fetching={isFetching}
+      onFetchNext={() => fetchNextPage({ page: nextPage })}
+      data={allData}
+      contentContainerStyle={{ paddingTop: top }}
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: Colors.SurfaceBackground,
-  },
-  contentContainer: {
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 5,
-  },
-  cellContainer: {
-    marginHorizontal: 5,
-    marginBottom: Spacing.m,
-  },
-});
