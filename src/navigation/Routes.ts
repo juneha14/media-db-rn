c4330d85595ef8d0be74505f71e437b8b5508556
June Ha <june.ha14@gmail.com>
2021-06-30 08:18:56 -0700

    Add StackRouteParamList and ModalRouteParamList to represent app's all possible screens and params

---
+++ src/navigation/Routes.ts
@@ -0,0 +1,19 @@
+import { Genre, SortOption } from "../models";
+import { KnownForMedia } from "../screens/Credits/utils";
+
+export type StackRouteParamList = {
+  MediaList: undefined;
+  MediaDetails: { id: number };
+  RecommendedList: { id: number };
+  CreditList: { id: number };
+  CreditDetails: { id: number };
+  CreditKnownForList: { media: KnownForMedia[] };
+  GenreDetails: { genre?: Genre; sortOption?: SortOption };
+  FavouriteList: undefined;
+};
+
+export type ModalRouteParamList = {
+  Filter: { option?: SortOption };
+};
+
+export type Screens = keyof StackRouteParamList | keyof ModalRouteParamList;
