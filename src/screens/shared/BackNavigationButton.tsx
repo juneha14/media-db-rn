e14eaae0ed79759c1a42d5e8873030aa8c443f65
June Ha <june.ha14@gmail.com>
2021-07-26 14:44:28 -0700

    Add cancel icon to shared BackNavigationButton

--- src/screens/shared/BackNavigationButton.tsx
+++ src/screens/shared/BackNavigationButton.tsx
@@ -1,20 +1,27 @@
 import React from "react";
-import { BackArrow } from "../../components/Icons";
+import { BackArrow, CancelIcon } from "../../components/Icons";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { StyleProp, ViewStyle } from "react-native";
 
 interface BackNavigationButtonProps {
+  type?: "back" | "cancel";
   onNavigateBack?: () => void;
 }
 
 export const BackNavigationButton: React.FC<BackNavigationButtonProps> = ({
+  type = "back",
   onNavigateBack,
 }) => {
   const { top, left } = useSafeAreaInsets();
-  return (
-    <BackArrow
-      style={{ position: "absolute", left: left + 15, top, zIndex: 100 }}
-      size="large"
-      onPress={onNavigateBack}
-    />
-  );
+  const style: StyleProp<ViewStyle> = {
+    position: "absolute",
+    left: left + 15,
+    top,
+    zIndex: 100,
+  };
+
+  if (type === "back")
+    return <BackArrow style={style} size="large" onPress={onNavigateBack} />;
+
+  return <CancelIcon style={style} size="large" onPress={onNavigateBack} />;
 };
