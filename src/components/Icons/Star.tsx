bf580d0a58a2d5ed618cfb6627f197b564999ac6
June Ha <june.ha14@gmail.com>
2021-10-19 13:46:31 -0700

    Update avatar and star icon component to fit add rating screen

--- src/components/Icons/Star.tsx
+++ src/components/Icons/Star.tsx
@@ -1,22 +1,35 @@
 import React from "react";
 import { StyleProp, ViewStyle, Pressable } from "react-native";
 import { Palette } from "../theme";
-import { Icon, IconSize } from "./Icon";
+import { IconSize } from "./Icon";
+import { IconLabel } from "./IconLabel";
 
 interface StarIconProps {
   size?: IconSize;
+  caption?: string;
+  encloseInBorder?: boolean;
   selected?: boolean;
   onPress?: () => void;
   style?: StyleProp<ViewStyle>;
 }
 
 export const StarIcon: React.FC<StarIconProps> = React.memo(
-  ({ size = "large", selected = false, onPress, style }) => {
+  ({
+    size = "large",
+    caption,
+    encloseInBorder = false,
+    selected = false,
+    onPress,
+    style,
+  }) => {
     return (
       <Pressable style={style} onPress={onPress}>
-        <Icon
+        <IconLabel
           name={selected ? "ios-star" : "ios-star-outline"}
           size={size}
+          label={caption}
+          labelPosition="below"
+          encloseInBorder={encloseInBorder}
           color={selected ? Palette.Yellow.L1 : Palette.White}
         />
       </Pressable>
